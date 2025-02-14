a72a10387fa8f824e21c453cbd5e387efeeef24e
bob frankel <bob@biosbob.biz>
2025-02-13 21:59:46 -0800

    emslexer fix

--- populate.sh
+++ populate.sh
@@ -5,6 +5,6 @@ mkdir docs/shelf
 cp docs/shelf-index.md docs/shelf/index.md
 
 pushd ../emscript-content/workspace > /dev/null
-emscript markdown -p em.core -o ../../emscript/emscript-docs/docs/shelf
-emscript markdown -p ti.cc23xx -o ../../emscript/emscript-docs/docs/shelf
+emscript-dev markdown -p em.core -o ../../emscript/emscript-docs/docs/shelf
+emscript-dev markdown -p ti.cc23xx -o ../../emscript/emscript-docs/docs/shelf
 popd > /dev/null
